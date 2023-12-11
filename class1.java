4165cc8906b8b9c049d13613f5a92963e6621c1e
muhammadshahms <muhammadshah4589@gmail.com>
2023-12-11 12:51:53 +0500

    first commit

---
+++ class1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class QuickStart {
+    public static void main(String[] args) {
+        String x = "hello";
+        String y = "world";
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        String temp = x;
+        x = temp;
+        x = y;
+        y = temp;
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+    }
+}
